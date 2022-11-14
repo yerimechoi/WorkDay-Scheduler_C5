e2c68eee2dc1e2094b236e08aedc2275a1f6d30d
yerimechoi <yerimechoi@gmail.com>
2022-11-14 03:26:07 -0500

    Doesn't save indefinitely to localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
 
-  $(".time-block").each(function(i){
+  $(".time-block").each(function(){
     var currentTime = dayjs().format("H");
     var idHour = $(this).attr("id");
 
@@ -13,20 +13,27 @@ $(document).ready(function(){
     }else if(currentTime < idHour){
       $(this).addClass("future");
     }
-    i++
   });
 
   $(".saveBtn").click(function(){
-    var userInput = $(this).sibling(".description").val();
-    var time = $(this).parent().attr("id");
+    var time = $(this).parents(".row").attr("id");
+    var userInput = $(this).siblings(".description").val();
 
-    localStorage.setItem(time, userInput);
+    save_localStorage(time, userInput);
   });
-  
+
+  function save_localStorage(time, userInput){
+    localStorage.setItem(time, userInput);
+
+    $(".time-block").children('textarea').attr("id");
+  };
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  var currentDate = dayjs().format("dddd, MMMM DD")
+
+  var currentDate = dayjs().format("dddd, MMMM DD h:m A")
   $("#currentDay").html(currentDate);
+
 });
