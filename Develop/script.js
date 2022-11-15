932deb3d2034eb56aff968ffadc8f1b191bcd641
yerimechoi <yerimechoi@gmail.com>
2022-11-15 18:33:34 -0500

    Changed localStorage.getItem syntax.

--- Develop/script.js
+++ Develop/script.js
@@ -20,9 +20,29 @@ $(document).ready(function(){
     localStorage.setItem(time, userInput);
   });
 
-  for(let i = 9; i < 18; i++){
-    $("#i").val(localStorage.getItem(i));
-  };
+  // for(var i=9; i<18; i++){
+  //     $(`#${i} .description`).val(localStorage.getItem(i));
+  // }
+
+  $("#hour-9 .description").val("working");
+
+  $("#hour-10 .description").val(localStorage.getItem(10));
+
+  $("#hour-11 .description").val(localStorage.getItem(11));
+
+  $("#hour-12 .description").val(localStorage.getItem(12));
+
+  $("#hour-13 .description").val(localStorage.getItem(13));
+
+  $("#hour-14 .description").val(localStorage.getItem(14));
+
+  $("#hour-15 .description").val(localStorage.getItem(15));
+
+  $("#hour-16 .description").val(localStorage.getItem(16));
+
+  $("#hour-17 .description").val(localStorage.getItem(17));
+
+  console.log(localStorage.getItem(9));
 
   var currentDate = dayjs().format("dddd, MMMM DD h:m A")
   $("#currentDay").html(currentDate);
