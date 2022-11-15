ad63f014ba564261c3df2661d5b58c7026430583
yerimechoi <yerimechoi@gmail.com>
2022-11-15 17:33:21 -0500

    User input is saved to localStorage.

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,28 @@
 $(document).ready(function(){
 
-  $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
-
   $(".time-block").each(function(){
     var currentTime = dayjs().format("H");
     var idHour = $(this).attr("id");
 
-    if(currentTime > idHour){
+    if(currentTime > idHour.split(" ")[1]){
       $(this).addClass("past");
-    }else if(currentTime == idHour){
+    }else if(currentTime == idHour.split(" ")[1]){
       $(this).addClass("present");
-    }else if(currentTime < idHour){
+    }else if(currentTime < idHour.split(" ")[1]){
       $(this).addClass("future");
     }
   });
 
   $(".saveBtn").click(function(){
-    var time = $(this).parents(".row").attr("id");
+    var time = $(this).parents(".row").attr("id").split(" ")[1];
     var userInput = $(this).siblings(".description").val();
 
-    save_localStorage(time, userInput);
+    localStorage.setItem(time, userInput);
   });
 
-  function save_localStorage(time, userInput){
-    localStorage.setItem(time, userInput);
-
-    $(".time-block").children('textarea').attr("id");
+  for(let i = 9; i < 18; i++){
+    $("#i").val(localStorage.getItem(i));
   };
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
 
   var currentDate = dayjs().format("dddd, MMMM DD h:m A")
   $("#currentDay").html(currentDate);
